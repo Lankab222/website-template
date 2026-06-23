65e44f6b64851494ca59723ea4e266645f683c0f
Lankab222 <m_lankab@hotmail.com>
2026-06-24 00:28:21 +0300

    update domin1

--- astro.config.mjs
+++ astro.config.mjs
@@ -50,7 +50,7 @@ function getAdapter() {
 }
 
 export default defineConfig({
-  site: 'https://www.taifalemar.site/',
+  site: 'https://taifalemar.site/',
   output: 'static',
   image: {
     domains: ['vitejs.dev', 'upload.wikimedia.org', 'astro.build', 'pagepro.co'],
