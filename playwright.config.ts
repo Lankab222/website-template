82f136be89ea7090ac45ce2b10cd8375555b29ab
Lankab222 <m_lankab@hotmail.com>
2026-06-30 16:56:55 +0300

    update npm update2

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'pnpm run dev',
+    command: 'npm run dev',
     url: 'http://localhost:4321',
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
